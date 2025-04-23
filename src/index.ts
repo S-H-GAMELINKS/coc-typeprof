89036cb2962e0cb936eafd11c8543b107e252336
S-H-GAMELINKS <gamelinks007@gmail.com>
2025-04-23 12:37:46 +0900

    Support Global install TypeProf LSP

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,59 @@
-import { ExtensionContext, services, workspace, LanguageClientOptions, LanguageClient, Uri } from 'coc.nvim'
+import cp from 'child_process';
+import fs from 'fs';
+import { ExtensionContext, services, workspace, LanguageClientOptions, LanguageClient, Uri, window, ServerOptions } from 'coc.nvim'
+
+function hasTyeProfInGemfileLock(): boolean {
+  try {
+    const gemfileLock = fs.readFileSync('Gemfile.lock', 'utf8');
+    return gemfileLock.includes('typeprof');
+  } catch {
+    return false;
+  }
+}
+
+function installedTypeProf(): Boolean {
+  try {
+    const result = cp.execSync('which typeprof', { stdio: 'pipe' }).toString();
+    if (result.match('typeprof')) {
+      return true;
+    }
+    return false;
+  } catch {
+    return true;
+  }
+}
+
+
+function getServerOptions(): ServerOptions | null {
+  if (hasTyeProfInGemfileLock()) {
+    return {
+      command: 'bundle',
+      args: [
+        'exec',
+        'typeprof',
+        '--stdio',
+        '--lsp'
+      ],
+    }
+  } else if (installedTypeProf()) {
+    return {
+      command: 'typeprof',
+      args: [
+        '--stdio',
+	'--lsp'
+      ]
+    }
+  } else {
+    window.showMessage('TypeProf is not installed! Please install TypeProf!!');
+    return null;
+  }
+}
 
 export async function activate(context: ExtensionContext): Promise<void> {
-  const serverOptions = {
-    command: 'bundle',
-    args: [
-      'exec',
-      'typeprof',
-      '--stdio',
-      '--lsp'
-    ],
+  const serverOptions = getServerOptions();
+
+  if (serverOptions == null) {
+    return;
   }
 
   const clientOptions: LanguageClientOptions = {
